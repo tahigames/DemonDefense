49ea42841f25e2566839fddbbef48a2757bcfec7
Marcel <marcelt91@live.de>
2015-04-14 10:41:11 +0200

    init

---
+++ android/src/de/tahigames/demondefense/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.tahigames.demondefense.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.tahigames.demondefense.DemonDefense;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DemonDefense(), config);
+	}
+}
---
+++ desktop/src/de/tahigames/demondefense/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.tahigames.demondefense.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.tahigames.demondefense.DemonDefense;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DemonDefense(), config);
+	}
+}

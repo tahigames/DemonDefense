f1ab3cafd7076aafb1f977ae71c0869b08bce708
MINZMANN <mirco.hinzmann@gmail.com>
2015-04-17 09:15:54 +0200

    Fix map being not centered at its position.

--- core/src/de/tahigames/demondefense/game/Map.java
+++ core/src/de/tahigames/demondefense/game/Map.java
@@ -10,6 +10,7 @@ import de.tahigames.demondefense.game.towers.BaseTower;
  * Created by Marcel on 14.04.2015.
  */
 public class Map extends Entity {
+
     public static final int cellSize = 16;
     private Cell[][] grid;
     private Cell selectedCell;
@@ -17,6 +18,7 @@ public class Map extends Entity {
 
     public Map(int cellsX, int cellsY) {
         super(0, 0);
+        validateCellCount(cellsX, cellsY);
         generateGrid(cellsX, cellsY);
         selectRenderer = new DrawComponent(new Texture("cells/testbox16selected.png"), Cell.SIZE, Cell.SIZE, DrawComponent.Layer.Eight);
     }
@@ -25,7 +27,7 @@ public class Map extends Entity {
         grid = new Cell[cellsX][cellsY];
         for (int x = 0; x < cellsX; x++) {
             for (int y = 0; y < cellsY; y++) {
-                Cell cell = new Cell(x * cellSize + cellSize / 2f, y * cellSize + cellSize / 2f);
+                Cell cell = new Cell((x - cellsX / 2) * cellSize + cellSize / 2f, (y - cellsY / 2) * cellSize + cellSize / 2f);
                 grid[x][y] = cell;
                 addChild(cell);
             }
@@ -33,14 +35,14 @@ public class Map extends Entity {
     }
 
     public Cell getCellAt(float x, float y){
-        return grid[(int) (x / cellSize)][(int) (y / cellSize)];
+        return grid[(int) ((x + getWidth() / 2) / cellSize)][(int) ((y + getHeight() /2) / cellSize)];
     }
 
     public void selectCellAt(float x, float y){
-        float mapWidth = cellSize * grid.length;
-        float mapHeight = cellSize * grid[0].length;
-        if(x >= getX() && x < getX() + mapWidth
-                && y >= getY() && y < getY() + mapHeight){
+        float halfMapWidth = getWidth() / 2;
+        float halfMapHeight = getHeight() / 2;
+        if(x >= getX() - halfMapWidth && x < getX() + halfMapWidth
+                && y >= getY() - halfMapHeight && y < getY() + halfMapHeight){
             if(selectedCell != null)
                 selectedCell.removeComponent(selectRenderer);
             selectedCell = getCellAt(x, y);
@@ -58,5 +60,20 @@ public class Map extends Entity {
             selectedCell.placeTower(new BaseTower());
             deselectCell();
         }
+    }
+
+    public float getWidth(){
+        return grid.length * cellSize;
+    }
+
+    public float getHeight(){
+        return grid[0].length * cellSize;
+    }
+
+    private void validateCellCount(int cellsX, int cellsY){
+        if(cellsX % 2 != 0)
+            throw new IllegalArgumentException("The amount of horizontal cells has to be even!");
+        if(cellsY % 2 != 0)
+            throw new IllegalArgumentException("The amount of vertical cells has to be even!");
     }
 }

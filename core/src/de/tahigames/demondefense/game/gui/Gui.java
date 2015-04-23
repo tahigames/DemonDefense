42e264c41fe905363a561843b28a315ed2ae4457
MHinzmann <hinzmann1993@yahoo.de>
2015-04-23 11:16:35 +0200

    Fix gui size.

--- core/src/de/tahigames/demondefense/game/gui/Gui.java
+++ core/src/de/tahigames/demondefense/game/gui/Gui.java
@@ -21,9 +21,9 @@ public class Gui extends Entity {
     public Gui(Level level, OrthographicCamera cam) {
         super((cam.viewportWidth * (2f/3f) * cam.zoom) / 2f, 0);
         this.map = level.getMap();
-        this.width = cam.viewportWidth / 3f * cam.zoom;
-        this.height = cam.viewportHeight * cam.zoom;
-        addComponent(new NinePatchComponent(new NinePatch(new Texture("gui/gui01.png"), 7, 0, 7, 7), width , height, RenderComponent.Realm.Gui, RenderComponent.Layer.Nine));
+        this.width = cam.viewportWidth / 3f;
+        this.height = cam.viewportHeight;
+        addComponent(new NinePatchComponent(new NinePatch(new Texture("gui/gui01.png"), 7, 0, 7, 7), width * cam.zoom, height * cam.zoom, RenderComponent.Realm.Gui, RenderComponent.Layer.Nine));
         addChild(new Button(getX(), getY()));
     }
 
